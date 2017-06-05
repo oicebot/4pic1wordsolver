f78fcd8e55c476e60bab00a2e63bd37bef53bca9
Oicebot <nervcn09@gmail.com>
2017-06-05 17:38:17 +0800

    add py2 version

--- wordfinder_py2.py
+++ wordfinder_py2.py
@@ -1,32 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 # -*- coding:utf-8 -*-
 
 required = 'dictionary'
 number = 3
 
-print('4Pic1Word作弊器 v1.1 by Oicebot @Guokr.com ',end='\n')
+print u'4Pic1Word作弊器 v1.1 by Oicebot @Guokr.com '
+print ' '
 
 while True:
-    print('请输入题中所给字母，不区分大小写，退出请直接回车：',end='')
-    required = input().lower()
+    print u'请输入题中所给字母，不区分大小写，退出请直接回车：',
+    required = str(raw_input()).lower()
     if len(required) < 1:
-        print('完毕，感谢使用。')
+        print u'完毕，感谢使用。'
         break
     if not required.isalpha():
-        print('输入错误，请重新输入！')
+        print u'输入错误，请重新输入！'
         continue
 
-    print('请输入目标单词的长度（3-10)：',end='')
+    print u'请输入目标单词的长度（3-10)：',
     try:
         number = int(input())
     except:
-        print('输入错误，请重新输入！')
+        print u'输入错误，请重新输入！'
         continue
     if number < 3:
         number = 3
-    print('--------')
-    print('使用字母来自：',required,'   ','搜索单词长度：',number)
-    print(' ')
+    print u'--------'
+    print u'使用字母来自：',required,'   ',u'搜索单词长度：',number
+    print ' '
     
     if number < 5:
         smldict = open("smallwords.txt","r")
@@ -38,7 +39,7 @@ while True:
         elif number == 4:
             dicts = word4
             
-        print('推荐单词：',end='')
+        print u'推荐单词：',
 
         line_len = 0
         for words in dicts:
@@ -47,12 +48,12 @@ while True:
                 if all(a in required for a in words):
                     if all(words.count(a) <= required.count(a) for a in words):
                         line_len += 1
-                        print(words,end='  ')
+                        print words,
                         if line_len % 8 == 0:
-                            print(' ')
+                            print ' '
                 
-        print(' ')
-        print(' ')
+        print ' '
+        print ' '
     with open("dictionary.txt","r") as dictionary:
         line_len = 0
         for line in dictionary:
@@ -61,9 +62,9 @@ while True:
                 if all(a in required for a in result):
                     if all(result.count(a) <= required.count(a) for a in result):
                         line_len += 1
-                        print(result,end='  ')
+                        print result,
                         if line_len % 8 == 0:
-                            print(' ')
-        print(' ')
-        print('--------')
+                            print ' '
+        print ' '
+        print '--------'
                     
